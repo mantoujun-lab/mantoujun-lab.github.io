4b78c2fe99d302701a1a5ecc8e1fcd486e6ee81b
mantoujun12 <3881679030@qq.com>
2026-08-11 23:34:07 +0800

    feat: add minify html swc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import minifyHtml from 'astro-minify-html-swc';
 
 // https://astro.build/config
 export default defineConfig({
+  // 注意：minifyHtml 必须放在 integrations 数组最后
+  integrations: [minifyHtml()],
   vite: {
     plugins: [tailwindcss()],
   },
